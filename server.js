b98dcb028a6b697a40f6339cfeae7aaad62d933e
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-05-24 19:29:36 +0200

    massive changes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const mongoose = require("mongoose")
+const User = require('./models/user');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const isAuthenticated = require('./middleware/authenticated');
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,24 +22,70 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 
+// Configurar el middleware de sesión con connect-mongo
+app.use(session({
+    secret: 'mysecret', // Cambia esto por una cadena secreta segura
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost:27017/tresenraya',
+        collectionName: 'sessions'
+    })
+}));
+
+// Conectar a MongoDB
+mongoose.connect('mongodb://localhost:27017/tresenraya')
+    .then(() => {
+        console.log('Conectado a MongoDB');
+    }).catch(err => {
+        console.error('Error al conectar a MongoDB', err);
+    });
+
+
 app.get("/login", (req, res) => {
     //res.sendFile(path.join(__dirname, 'public', 'login.html'));
-    let error="";
-    res.render('login',{error});
+    const error = req.query.error || '';;
+    res.render('login', { error });
+})
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.render('login', { error: 'Usuario o contraseña incorrecto' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.render('login', { error: 'Usuario o contraseña incorrecto' });
+        }
+        req.session.user=user;
+        res.redirect('/juego');
+    } catch (error) {
+        console.error(error);
+        res.render('login', { error: 'Error de conexion a la base de datos' });
+    }
 
 })
 
-app.post("/login", (req, res) => {
-    const {email,password}=req.body;
-    if(email=="admin@gmail.com" & password=="1234"){
-        res.redirect("/juego");
-    }else{
-        res.render('login', { error: 'Usuario o password incorrecta'});
+app.get("/register",(req,res)=>{
+    let error="";
+    res.render("register",{error}); 
+})
 
+app.post("/register", async (req, res) => {
+    const {name,email,password}=req.body;
+    let usuario = new User({ name, email, password });
+    try {
+        await usuario.save();
+        res.redirect("/login");
+    } catch (error) {      
+        res.render("register",{error:"Error de conexion a bbdd"});
     }
 })
 
-app.get("/juego",(req,res)=>{
+app.get("/juego", isAuthenticated,(req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'juego.html'));
 })
 
