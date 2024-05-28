47735435b94e82b76194ddda1d706ac217fd10db
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-05-28 20:07:22 +0200

    edicion 1

--- public/js/juego.js
+++ public/js/juego.js
@@ -3,17 +3,40 @@ let connectedUsers = document.getElementById("connectedUsers");
 socket.on('pendientes', (datos) => {
     console.log(datos);
 })
+
+socket.on('mensaje', (datos) => {
+    const p = document.createElement('p');
+    p.innerText = datos.name + ": " + datos.mensaje;
+    document.getElementById("chatgeneral").appendChild(p);
+})
+socket.on('privados', (datos) => {
+    const p = document.createElement('p');
+    p.innerText = datos;
+    document.getElementById("chatprivado").appendChild(p);
+    //Creo partida pendiente
+    const li = document.createElement('li');
+    li.textContent = "partida de" + datos.name;
+    li.classList.add('list-group-item');
+    document.getElementById("partidasPendientes").appendChild(li);
+
+})
 socket.on('usuarios', (datos) => {
     connectedUsers.innerHTML = "";
     datos.forEach(user => {
         if (!document.getElementById(user._id)) {
             const li = document.createElement('li');
             li.id = user.socketId;
+            li.setAttribute('data-id', user._id);
             li.textContent = user.name;
             li.classList.add('list-group-item');
             li.onclick = (e) => {
-                alert(e.currentTarget.id)
-                socket.emit("invitaciones", e.currentTarget.id);
+                let datos = {
+                    userId: e.target.dataset.id,
+                    socketID: e.currentTarget.id,
+                    name: e.currentTarget.innerHTML
+                }
+                console.log(datos)
+                socket.emit("invitaciones", datos);
             }
             connectedUsers.appendChild(li);
         }
@@ -26,5 +49,6 @@ socket.on('usuarios', (datos) => {
 
 document.getElementById("btnEnviar").onclick = () => {
     let texto = document.getElementById("texto").value;
+    document.getElementById("texto").value = "";
     socket.emit("mensaje", texto);
 }
